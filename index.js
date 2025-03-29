745484f15d65a5334bfdab20d6009d85ac7858cc
alasiri <austinalasiri44@gmail.com>
2025-03-29 12:55:23 +0300

    finished creating the new classes

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+// Creates a class of breakfast
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+// Creates a class of lunch
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup  = soup;
+        this.drink = drink;
+    }
+}
+
+// Creates a class of dinner
+class Dinner{
+    // Dessert is a private field
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad  = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
